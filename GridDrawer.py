e8a4d51f29b705e36bfac3b0b743f36be8d014cb
Deniss Fjodorovs <3starblaze@gmail.com>
2020-03-19 20:26:35 +0200

    Move drawing code to a class
    That way we are separating concerns and hiding implementation details.

---
+++ GridDrawer.py
@@ -0,0 +1,53 @@
+import pyglet
+from pyglet.gl import GL_LINES, GL_QUADS
+
+class GridDrawer:
+    def __init__(self, line_width, spacing, data):
+        self.window = pyglet.window.Window()
+        self.width, self.height = self.window.get_size()
+
+        self.line_width = line_width
+        pyglet.gl.glLineWidth(self.line_width)
+        self.spacing = spacing
+        self.data = data
+
+
+    def draw_grid(self):
+        # Horizontal lines
+        for i in range(self.line_width//2, self.height, self.spacing):
+            pyglet.graphics.draw(2, GL_LINES,
+                                 ('v2i', (0, i, self.width, i)),
+                                 ('c3B', (0, 100, 100) * 2))
+        # Vertical lines
+        for i in range(self.line_width//2, self.width, self.spacing):
+            pyglet.graphics.draw(2, GL_LINES,
+                                 ('v2i', (i, 0, i, self.height)),
+                                 ('c3B', (0, 100, 100) * 2))
+
+
+    def draw_cells(self):
+        starting_points = []
+        for i, row in enumerate(self.data):
+            for j, elem in enumerate(row):
+                if elem:
+                    starting_points.append((j, i))
+
+        for x, y in starting_points:
+            cell_size = self.spacing - self.line_width
+            point1 = (self.line_width * (x + 1) + cell_size * x,
+                      self.line_width * (y + 1) + cell_size * y)
+            point2 = (point1[0] + cell_size, point1[1])
+            point3 = (point2[0], point2[1] + cell_size)
+            point4 = (point3[0] - cell_size, point3[1])
+            pyglet.graphics.draw(4, GL_QUADS,
+                                 ('v2i', (*point1, *point2, *point3, *point4)),
+                                 ('c3B', (114, 109, 168) * 4))
+
+
+    def start(self):
+        @self.window.event
+        def on_draw():
+            self.draw_grid()
+            self.draw_cells()
+
+        pyglet.app.run()
--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-import pyglet
-from pyglet.gl import GL_LINES
+from GridDrawer import GridDrawer
 
+my_list = [[False, False, True],
+           [False, True, False],
+           [True, False, False],
+           [True] * 3,
+           [False, True] * 4]
 
-def draw_grid(spacing, width, height):
-    # Horizontal lines
-    for i in range(0, height, spacing):
-        pyglet.graphics.draw(2, GL_LINES,
-                              ('v2i', (0, i, width, i)))
-    # Vertical lines
-    for i in range(0, width, spacing):
-        pyglet.graphics.draw(2, GL_LINES,
-                              ('v2i', (i, 0, i, height)))
-
-
-window = pyglet.window.Window()
-w, h = window.get_size()
-
-@window.event
-def on_draw():
-    draw_grid(10, w, h)
-
-pyglet.app.run()
+MyGrid = GridDrawer(4, 30, my_list)
+MyGrid.start()

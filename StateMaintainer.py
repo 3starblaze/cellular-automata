171d5dc1b754f424169a506819dde96e67781339
Deniss Fjodorovs <3starblaze@gmail.com>
2020-03-21 15:39:56 +0200

    Add rules parameter
    Includes setter/getter with an emptiness check.
    Also refactored old tests which didn't pass 'rules' argument before.

--- StateMaintainer.py
+++ StateMaintainer.py
@@ -2,8 +2,9 @@ import numpy as np
 
 
 class StateMaintainer:
-    def __init__(self, data):
+    def __init__(self, data, rules):
         self.data = data
+        self.rules = rules
 
 
     @property
@@ -18,3 +19,14 @@ class StateMaintainer:
             raise ValueError("data expected in 2 dimensions; got {temp_data.ndim}")
 
         self._data = temp_data
+
+
+    @property
+    def rules(self):
+        return self.rules
+
+    @rules.setter
+    def rules(self, value):
+        if value == {}:
+            raise ValueError("rules is empty!")
+        self._rules = value
--- StateMaintainer.test.py
+++ StateMaintainer.test.py
@@ -2,24 +2,61 @@ import unittest
 
 from StateMaintainer import StateMaintainer
 
+
+def GAME_OF_LIFE_RULESET(cell, values):
+    if cell:
+        return values.sum() >= 2 and values.sum() <= 3
+    else:
+        return values.sum() == 3
+
+
+# One of the valid rules
+GAME_OF_LIFE = {
+    'indices': [(-1, 1),  (0, 1),  (1, 1),
+                (-1, 0),           (1, 0),
+                (-1, -1), (0, -1), (1, -1),],
+    'ruleset': GAME_OF_LIFE_RULESET
+}
+
+VALID_DATA = [[False, False, True],
+              [False, True, False],
+              [True, False, False]]
+
+
 class TestStateMaintainer(unittest.TestCase):
+    # 'data' argument tests
     def test_incorrectly_shaped_1d_input(self):
         self.assertRaises(ValueError, StateMaintainer,
-                          [False, False, True, False, True, True])
+                          [False, False, True, False, True, True],
+                          GAME_OF_LIFE)
 
 
     def test_correctly_shaped_2d_input(self):
         try:
             StateMaintainer([[True, False, False],
                              [False, True, False],
-                             [False, False, True]])
+                             [False, False, True]],
+                            GAME_OF_LIFE)
         except ValueError:
             self.fail("Correctly shapped array raises an exception!")
 
 
     def test_incorrectly_shaped_3d_input(self):
         self.assertRaises(ValueError, StateMaintainer,
-                          [[[False, False]], [[True, False]], [[True, True]]])
+                          [[[False, False]], [[True, False]], [[True, True]]],
+                          GAME_OF_LIFE)
+
+
+    # 'rules' argument tests
+    def test_correct_rule(self):
+        try:
+            StateMaintainer(VALID_DATA, GAME_OF_LIFE)
+        except ValueError:
+            self.fail("Correct rule raises an exception!")
+
+
+    def test_empty_rule(self):
+        self.assertRaises(ValueError, StateMaintainer, VALID_DATA, {})
 
 
 if __name__ == '__main__':

0e95489831f2de2cdfe7f62238a969892fcdeb58
Deniss Fjodorovs <3starblaze@gmail.com>
2020-03-21 14:04:05 +0200

    Create setter/getter and move validation in setter
    Since we intend data to be mutable, it has to be checked in setter and thus
    doesn't need to be check in __init__.

--- StateMaintainer.py
+++ StateMaintainer.py
@@ -3,8 +3,18 @@ import numpy as np
 
 class StateMaintainer:
     def __init__(self, data):
-        data = np.array(data)
-        if data.ndim != 2:
-            raise ValueError("data expected in 2 dimensions; got {data.ndim}")
+        self.data = data
 
-        self.data = np.array(data)
+
+    @property
+    def data(self):
+        return self.data
+
+
+    @data.setter
+    def data(self, value):
+        temp_data = np.array(value)
+        if temp_data.ndim != 2:
+            raise ValueError("data expected in 2 dimensions; got {temp_data.ndim}")
+
+        self._data = temp_data

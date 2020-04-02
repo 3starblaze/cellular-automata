e942d0de39d69de722031c2a9520b7b88dcdf3a1
Deniss Fjodorovs <3starblaze@gmail.com>
2020-04-02 19:51:06 +0300

    Add drawer object parameter
    Also updates tests that start to fail because of these changes.

--- src/Controller.py
+++ src/Controller.py
@@ -1,11 +1,14 @@
+import pyglet
+
 from GridDrawer import GridDrawer
 from StateMaintainer import StateMaintainer
 
 
 class Controller:
-    def __init__(self, State, iterations):
+    def __init__(self, State, Drawer, iterations):
         if not issubclass(type(State), StateMaintainer):
             raise ValueError("State is not an instance of StateMaintainer!")
+
         try:
             if iterations != int(iterations):
                 raise ValueError()
@@ -14,10 +17,20 @@ class Controller:
         if iterations <= 0:
             raise ValueError("iterations is not positive!")
 
+        try:
+            if callable(Drawer.draw_grid) and callable(Drawer.draw_cells):
+                raise ValueError()
+        except (ValueError, AttributeError) as e:
+            raise ValueError(
+                "`Drawer` doesn't have callable functions `draw_grid` and/or `draw_cells`"
+            )
+
         self.State = State
         self.iterations = iterations
 
+        # TODO Attach events that:
+        # draw grid
+        # allow changing states while pushing keys
+
     def run(self):
-        for i in range(self.iterations):
-            GridDrawer(2, 30, self.State.data).start()
-            self.State.apply_rules()
+        pyglet.app.run()
--- src/test_Controller.py
+++ src/test_Controller.py
@@ -1,29 +1,48 @@
 import unittest
 
 from Controller import Controller
+from GridDrawer import GridDrawer
 from StateMaintainer import StateMaintainer
 from test_StateMaintainer import VALID_DATA, GAME_OF_LIFE
 
 VALID_STATE_MAINTAINER = StateMaintainer(VALID_DATA, GAME_OF_LIFE)
+VALID_GRID_DRAWER = GridDrawer(2, 10, [])
 
 
 class TestController(unittest.TestCase):
     def test_incorrect_data_type_for_State(self):
-        self.assertRaisesRegex(ValueError, "State", Controller, "lasagna", 10)
+        self.assertRaisesRegex(
+            ValueError, "State", Controller, "lasagna", VALID_GRID_DRAWER, 10
+        )
 
     def test_incorrect_data_type_for_iterations(self):
         self.assertRaisesRegex(
-            ValueError, "iterations", Controller, VALID_STATE_MAINTAINER, 4.72
+            ValueError,
+            "iterations",
+            Controller,
+            VALID_STATE_MAINTAINER,
+            VALID_GRID_DRAWER,
+            4.72,
         )
 
     def test_negative_iterations(self):
         self.assertRaisesRegex(
-            ValueError, "iterations", Controller, VALID_STATE_MAINTAINER, -4
+            ValueError,
+            "iterations",
+            Controller,
+            VALID_STATE_MAINTAINER,
+            VALID_GRID_DRAWER,
+            -4,
         )
 
     def test_0_iterations(self):
         self.assertRaisesRegex(
-            ValueError, "iterations", Controller, VALID_STATE_MAINTAINER, 0
+            ValueError,
+            "iterations",
+            Controller,
+            VALID_STATE_MAINTAINER,
+            VALID_GRID_DRAWER,
+            0,
         )
 
 

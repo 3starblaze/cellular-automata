c85ade0daa7ca0f72cac71a71e28fe4208bb0573
Deniss Fjodorovs <3starblaze@gmail.com>
2020-04-29 18:03:09 +0300

    Change how Controller draws
    To accustom with GridDrawer's changes.

--- src/Controller.py
+++ src/Controller.py
@@ -1,6 +1,6 @@
 import pyglet
 from pyglet.window import key
-from pyglet.gl import GL_QUADS
+from pyglet.gl import GL_QUADS, GLubyte
 
 from GridDrawer import GridDrawer
 from StateMaintainer import StateMaintainer
@@ -22,30 +22,24 @@ class Controller:
         self.State = State
 
         self.window = pyglet.window.Window()
+        self._drawn_data = Drawer.draw(*self.window.get_size())
 
         @self.window.event
         def on_draw():
             self.window.clear()
-            for line_tuple in Drawer.draw_grid(*self.window.get_size()):
-                pyglet.graphics.draw(
-                    4,
-                    GL_QUADS,
-                    ("v2i", line_tuple),
-                    ("c3B", Drawer.grid_line_color * 4),
-                )
-            for cell_block in Drawer.draw_cells():
-                pyglet.graphics.draw(
-                    4,
-                    GL_QUADS,
-                    ("v2i", cell_block),
-                    ("c3B", Drawer.grid_cell_color * 4),
-                )
+            # data needs to be in ctype, otherwise can't be blitted
+            raw_data = (GLubyte * self._drawn_data.size)(
+                *self._drawn_data.flatten().astype("int")
+            )
+            img = pyglet.image.ImageData(*self._drawn_data.shape[:2], "RGB", raw_data)
+            img.blit(0, 0)
 
         @self.window.event
         def on_key_press(symbol, modifiers):
             if symbol == key.RIGHT:
                 State.apply_rules()
                 Drawer.data = State.data
+                self._drawn_data = Drawer.draw(*self.window.get_size())
 
     def run(self):
         pyglet.app.run()

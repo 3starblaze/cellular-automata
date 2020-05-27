f6b5a4eb62b1e982ce6595c9fccef7ceeff0566e
Deniss Fjodorovs <3starblaze@gmail.com>
2020-05-27 18:09:12 +0300

    Convert to pytest
    Also remove unittest dependencies.

--- src/test_GridDrawer.py
+++ src/test_GridDrawer.py
@@ -1,7 +1,7 @@
-import unittest
 import random
 
 import numpy as np
+import pytest
 
 from GridDrawer import GridDrawer
 
@@ -9,132 +9,100 @@ VALID_COLOR = (0, 100, 200)
 VALID_DATA = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
 
 
-class TestGridDrawer(unittest.TestCase):
-    def test_negative_line_width(self):
-        self.assertRaisesRegex(
-            ValueError,
-            "line_width",
-            GridDrawer,
-            random.randint(-100, -1),
-            10,
-            VALID_DATA,
-        )
-
-    def test_0_line_width(self):
-        try:
-            GridDrawer(0, 10, VALID_DATA)
-        except ValueError:
-            self.fail("0 line width raises an exception!")
-
-    def test_negative_cell_size(self):
-        self.assertRaisesRegex(
-            ValueError, "cell_size", GridDrawer, 2, random.randint(-100, -1), []
-        )
-
-    def test_0_cell_size(self):
-        self.assertRaisesRegex(ValueError, "cell_size", GridDrawer, 2, 0, [])
-
-    def test_data_getter(self):
-        received_data = GridDrawer(2, 10, VALID_DATA).data.tolist()
-        self.assertEqual(received_data, VALID_DATA)
-
-    def test_incorrectly_shaped_1d_input(self):
-        self.assertRaisesRegex(
-            ValueError,
-            "data",
-            GridDrawer,
-            2,
-            10,
-            [False, False, True, False, True, True],
-        )
-
-    def test_correctly_shaped_2d_input(self):
-        try:
-            GridDrawer(
-                2,
-                10,
-                [[True, False, False], [False, True, False], [False, False, True]],
-            )
-        except ValueError:
-            self.fail("Correctly shapped array raises an exception!")
-
-    def test_incorrectly_shaped_3d_input(self):
-        self.assertRaisesRegex(
-            ValueError,
-            "data",
-            GridDrawer,
-            2,
-            10,
-            [[[False, False]], [[True, False]], [[True, True]]],
-        )
-
-    def test_incorect_color_format(self):
-        self.assertRaisesRegex(
-            ValueError, "color format", GridDrawer, 2, 10, VALID_DATA, "skrrr"
-        )
-        self.assertRaisesRegex(
-            ValueError, "color format", GridDrawer, 2, 10, VALID_DATA, VALID_COLOR, 40
-        )
-
-    def test_incorrectly_shaped_color_tuples(self):
-        self.assertRaisesRegex(
-            ValueError, "color format", GridDrawer, 2, 10, VALID_DATA, (4, 2)
-        )
-        self.assertRaisesRegex(
-            ValueError,
-            "color format",
-            GridDrawer,
-            2,
-            10,
-            VALID_DATA,
-            VALID_COLOR,
-            (20, 40, 80, 20),
-        )
-
-    def test_colors_with_invalid_numbers(self):
-        self.assertRaisesRegex(
-            ValueError, "color format", GridDrawer, 2, 10, VALID_DATA, (400, 20, 50)
-        )
-        self.assertRaisesRegex(
-            ValueError,
-            "color format",
-            GridDrawer,
-            2,
-            10,
-            VALID_DATA,
-            VALID_COLOR,
-            (-10, 80, 90),
-        )
-
-    def test_colors_with_floats(self):
-        self.assertRaisesRegex(
-            ValueError,
-            "color format",
-            GridDrawer,
-            2,
-            10,
-            VALID_DATA,
-            (10.23, 20.21, 50),
-        )
-        self.assertRaisesRegex(
-            ValueError,
-            "color format",
-            GridDrawer,
-            2,
-            10,
-            VALID_DATA,
-            VALID_COLOR,
-            (43.92, 123.45, 92.2),
-        )
-
-    def test_drawer(self):
-        data = [[1, 1, 0], [0, 1, 1], [1, 0, 1]]
-        width, height = (10, 12)
-        MyDrawer = GridDrawer(2, 4, data)
-        returned_object = MyDrawer.draw(width, height)
-        assert len(returned_object["lines"]) == 4, "Line count doesn't match"
-        assert len(returned_object["cells"]) == 4, "Cell count doesn't match"
+def test_negative_line_width():
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(random.randint(-100, -1), 10, VALID_DATA)
+    assert "line_width" in str(excinfo.value), "negative line_width is accepted"
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_0_line_width():
+    assert GridDrawer(0, 10, VALID_DATA), "line_width = 0 isn't accepted"
+
+
+def test_negative_cell_size():
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, random.randint(-100, -1), [])
+    assert "cell_size" in str(excinfo.value), "negative cell_size is accepted"
+
+
+def test_0_cell_size():
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 0, [])
+    assert "cell_size" in str(excinfo.value), "cell_size = 0 is accepted"
+
+
+def test_data_getter():
+    received_data = GridDrawer(2, 10, VALID_DATA).data
+    np.testing.assert_equal(received_data, VALID_DATA)
+
+
+def test_incorrectly_shaped_1d_input():
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 10, [0, 0, 1, 0, 1, 1])
+    assert "data" in str(excinfo.value), "1D data accepted"
+
+
+def test_correctly_shaped_2d_input():
+    GridDrawer(
+        2, 10, [[True, False, False], [False, True, False], [False, False, True]],
+    )
+    assert True, "2D input not accepted"
+
+
+def test_incorrectly_shaped_3d_input():
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 10, [[[False, False]], [[True, False]], [[True, True]]])
+    assert "data" in str(excinfo.value), "3D data accepted"
+
+
+def test_incorect_color_format():
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 10, VALID_DATA, "skrrr")
+    assert "color format" in str(excinfo.value), "Wrong color format accepted"
+
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 10, VALID_DATA, VALID_COLOR, 40)
+    assert "color format" in str(excinfo.value), "Wrong color format accepted"
+
+
+def test_incorrectly_shaped_color_tuples():
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 10, VALID_DATA, (4, 2))
+    assert "color format" in str(excinfo.value), "Wrongly shaped color format accepted"
+
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 10, VALID_DATA, VALID_COLOR, (20, 40, 80, 20))
+    assert "color format" in str(excinfo.value), "Wrongly shaped color format accepted"
+
+
+def test_colors_with_invalid_numbers():
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 10, VALID_DATA, (400, 20, 50))
+    assert "color format" in str(
+        excinfo.value
+    ), "Color format with wrong integers accepted"
+
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 10, VALID_DATA, VALID_COLOR, (-10, 80, 90))
+    assert "color format" in str(
+        excinfo.value
+    ), "Color format with wrong integers accepted"
+
+
+def test_colors_with_floats():
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 10, VALID_DATA, (10.23, 20.21, 50))
+    assert "color format" in str(excinfo.value), "Color format with floats accepted"
+
+    with pytest.raises(ValueError) as excinfo:
+        GridDrawer(2, 10, VALID_DATA, VALID_COLOR, (43.92, 123.45, 92.2))
+    assert "color format" in str(excinfo.value), "Color format with floats accepted"
+
+
+def test_drawer():
+    data = [[1, 1, 0], [0, 1, 1], [1, 0, 1]]
+    width, height = (10, 12)
+    MyDrawer = GridDrawer(2, 4, data)
+    returned_object = MyDrawer.draw(width, height)
+    assert len(returned_object["lines"]) == 4, "Line count doesn't match"
+    assert len(returned_object["cells"]) == 4, "Cell count doesn't match"

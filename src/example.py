6b94059f284e200f8d809a4a5c3e87f7da43aca9
3starblaze <47479439+3starblaze@users.noreply.github.com>
2020-06-04 10:20:35 +0300

    Update example.py according to `Quickstart`
    Some bits of code didn't match with the `Quickstart` and thus had to be changed.

--- src/example.py
+++ src/example.py
@@ -2,7 +2,7 @@ from StateMaintainer import StateMaintainer
 from GridDrawer import GridDrawer
 from Controller import Controller
 
-my_list = [[0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 0, 0], [0, 0, 0]]
+my_list = np.array([random.choice([1, 0]) for i in range(20**2)]).reshape(20, 20)
 
 
 def GAME_OF_LIFE_RULESET(cell, values):
@@ -19,6 +19,6 @@ GAME_OF_LIFE = {
 
 
 MyState = StateMaintainer(my_list, GAME_OF_LIFE)
-MyDrawer = GridDrawer(2, 30, MyState.data)
+MyDrawer = GridDrawer(1, 30, MyState.data)
 MyController = Controller(MyState, MyDrawer)
 MyController.run()

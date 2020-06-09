30018c46a7bdfe248bc1e3f0f512f912c2f0d5f7
Deniss Fjodorovs <3starblaze@gmail.com>
2020-06-09 10:13:46 +0300

    Create main functions for Controller
    Those provide the base functionality to Controller.

--- src/Controller.py
+++ src/Controller.py
@@ -1,7 +1,22 @@
-class Controller:
-    def __init__(self, rule, data=None):
-        self.rule = rule
-        self.data = data
+from StateMaintainer import StateMaintainer
+from GridDrawer import GridDrawer
+from Rule import Rule
 
-    def run():
-        pass
+
+class Controller:
+    """Provide a simple interface for cellular automata simulations."""
+
+    def __init__(self, indices, ruleset, data=None):
+        self.data = data
+        self.state = StateMaintainer(data, Rule(indices, ruleset))
+        self.drawer = GridDrawer(2, 30, data)
+        self.drawing_data = {}
+
+    def _update_data(self, data, width, height):
+        self.state.data = data
+        self.drawer.data = data
+        self.drawing_data = self.drawer.draw(width, height)
+
+    def next_frame(self, width, height):
+        StateMaintainer.apply_rule()
+        self._update_data(StateMaintainer.data, width, height)

b11672b9fa53b28095e9eabad58f034481767fb7
Deniss Fjodorovs <3starblaze@gmail.com>
2020-06-09 10:22:20 +0300

    Make `width` and `height` as instance variables
    Since `Controller`'s main job is to be as simple as possible, it's better
    to make `width` and `height` to be instance variables with defaults and
    opportunity to change them if needed. That makes it easy to call methods
    because they don't need any additional arguments.

--- src/Controller.py
+++ src/Controller.py
@@ -6,17 +6,19 @@ from Rule import Rule
 class Controller:
     """Provide a simple interface for cellular automata simulations."""
 
-    def __init__(self, indices, ruleset, data=None):
+    def __init__(self, indices, ruleset, width=100, height=100, data=None):
+        self.width = width
+        self.height = height
         self.data = data
         self.state = StateMaintainer(data, Rule(indices, ruleset))
         self.drawer = GridDrawer(2, 30, data)
         self.drawing_data = {}
 
-    def _update_data(self, data, width, height):
+    def _update_data(self, data):
         self.state.data = data
         self.drawer.data = data
-        self.drawing_data = self.drawer.draw(width, height)
+        self.drawing_data = self.drawer.draw(self.width, self.height)
 
-    def next_frame(self, width, height):
+    def next_frame(self):
         StateMaintainer.apply_rule()
-        self._update_data(StateMaintainer.data, width, height)
+        self._update_data(StateMaintainer.data)

4786325a6ea560543f0b3cf6e51e867d928f2607
Deniss Fjodorovs <3starblaze@gmail.com>
2020-06-22 12:50:18 +0300

    Add temporary 'GET' page for '/controller'

--- src/WebController.py
+++ src/WebController.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Markup
 from Controller import Controller
 from Rule import Rule
 
@@ -7,14 +7,20 @@ app = Flask(__name__)
 current_controller = {}
 
 
-@app.route("/controller", methods=["POST"])
+@app.route("/controller", methods=["GET", "POST"])
 def controller_route_handler():
     global current_controller
+    if request.method == "GET":
+        if current_controller:
+            return Markup.escape(str(current_controller))
+        else:
+            return "There is no controller at the moment!"
+
     if request.method == "POST":
         data = request.get_json(force=True)
         try:
             kwargs = data.get("kwargs") or {}
-            ruleset_string = kwargs.get('ruleset')
+            ruleset_string = kwargs.get("ruleset")
             kwargs["ruleset"] = Rule.string_to_ruleset(ruleset_string)
             current_controller = Controller(**kwargs)
             return {"success": True}

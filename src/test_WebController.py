388311866a7a8a3e20f1683cfdff3ae8b0437300
Deniss Fjodorovs <3starblaze@gmail.com>
2020-06-26 15:09:07 +0300

    Add basic tests for all routes

--- src/test_WebController.py
+++ src/test_WebController.py
@@ -9,9 +9,42 @@ def client():
     return WebController.test_client()
 
 
+@pytest.fixture
+def controller_init(client):
+    return client.post(
+        "/controller",
+        json={
+            "kwargs": {
+                "indices": [[0, 1], [1, 0]],
+                "ruleset": "if cell:\n    return 3 <= sum(retrieved_cells) <= 4\nelse:\n    return sum(retrieved_cells) == 3",
+                "width": 467,
+            }
+        },
+    )
+
+
 def test_index_page(client):
     assert client.get("/").status_code == 404
 
 
 def test_controller_page(client):
     assert client.get("/controller").data == b"There is no controller at the moment!"
+
+
+def test_controller_creation(client, controller_init):
+    data = controller_init.get_json()
+    assert data["success"]
+    assert client.get("/controller").data != b"There is no controller at the moment!"
+
+
+def test_prop_access(client, controller_init):
+    assert client.get("/controller/width").get_json()["prop"] == 467
+    assert client.get("/controller/data").get_json()["prop"] == [[0] * 10] * 10
+
+
+def test_prop_update(client, controller_init):
+    assert (
+        client.post("/controller/update", json={"height": 420}).get_json()["success"]
+        == True
+    )
+    assert client.get("/controller/height").get_json()["prop"] == 420
